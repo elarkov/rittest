d3fe11b77f5179ab63b18a3083069d128719b91d
Evgeny Larkov <elarkov@inbox.ru>
2018-06-28 00:13:07 +0700

    minor change files

--- src/js/animateTD.js
+++ src/js/animateTD.js
@@ -2,23 +2,23 @@ export let result;
 
 function animateTD(){
 
-  let tableCell = document.querySelectorAll('.table__cell');
-  let text = document.querySelector('.result__text');
-      result = document.querySelector('.result');
+	let tableCell = document.querySelectorAll('.table__cell');
+	let text = document.querySelector('.result__text');
+	result = document.querySelector('.result');
 
-  for(let i = 0; i < tableCell.length; i++){
-    tableCell[i].addEventListener('click', function() {
-      this.style.fontSize = '20px';
-      this.style.color = '#fff';
-      this.style.backgroundColor = '#ff5722';
-      text.style.display = 'none';
-      result.innerHTML += `<p class="cell-text"> ${tableCell[i].textContent}`;
-    });
-  }
+	for(let i = 0; i < tableCell.length; i++){
+		tableCell[i].addEventListener('click', function() {
+			this.style.fontSize = '20px';
+			this.style.color = '#fff';
+			this.style.backgroundColor = '#ff5722';
+			text.style.display = 'none';
+			result.innerHTML += `<p class="cell-text"> ${tableCell[i].textContent}`;
+		});
+	}
 
 }
 
 
 export {
-  animateTD
-}
+	animateTD
+};
--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,27 +1,27 @@
-import {result} from "./animateTD";
+import {result} from './animateTD';
 
 function buttons(){
 
-  let table = document.querySelector('.table'),
-      tableTwo = document.querySelector('.table-two'),
-      btnOne = document.querySelector('.btn-one'),
-      btnTwo = document.querySelector('.btn-two');
+	let table = document.querySelector('.table'),
+		tableTwo = document.querySelector('.table-two'),
+		btnOne = document.querySelector('.btn-one'),
+		btnTwo = document.querySelector('.btn-two');
 
-      btnOne.addEventListener('click', () => {
-        table.style.display = 'none';
-        tableTwo.style.display = 'flex';
-        result.style.display = 'none';
-      });
+	btnOne.addEventListener('click', () => {
+		table.style.display = 'none';
+		tableTwo.style.display = 'flex';
+		result.style.display = 'none';
+	});
 
-      btnTwo.addEventListener('click', () => {
-        table.style.display = 'table';
-        tableTwo.style.display = 'none';
-        result.style.display = 'flex';
-      });
+	btnTwo.addEventListener('click', () => {
+		table.style.display = 'table';
+		tableTwo.style.display = 'none';
+		result.style.display = 'flex';
+	});
 
 }
 
 
 export {
-  buttons
-}
+	buttons
+};
